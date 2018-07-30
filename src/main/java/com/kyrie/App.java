a49d3a1b531c58abce8000b1f03ef7bc427086bc
changyue.li <changyue.li@tendcloud.com>
2018-07-30 16:43:56 +0800

    Initial commit
    Change-Id: I575b5694971ae2a1924757627e0d4b0a1d2f2e0a

---
+++ src/main/java/com/kyrie/App.java
@@ -0,0 +1,13 @@
+package com.kyrie;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
