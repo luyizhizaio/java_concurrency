369c1044f4075f96b6a2b0a375e7735c72b4d131
changyue.li <changyue.li@tendcloud.com>
2019-09-16 18:08:21 +0800

    散列表-线性探测表
    Change-Id: Ibfa7e55af72ec8f40100323eca61b24506fdd54b

---
+++ src/main/java/com/kyrie/datastructure/search/LinearProbingHashST.java
@@ -0,0 +1,85 @@
+package com.kyrie.datastructure.search;
+
+/**
+ * Created by tend on 2019/9/16.
+ * 线性探测符号表
+ */
+public class LinearProbingHashST<Key,Value> {
+
+
+    private int N; //符号表这种键值对的总数
+    private int M; //表的总大小
+    private Key[] keys;
+    private Value[] vals;
+
+    public LinearProbingHashST(){
+        keys = (Key[]) new Object[M];
+        vals = (Value[]) new Object[M];
+    }
+
+    private void resize(int M){
+
+    }
+
+    private int hash(Key key){
+        return (key.hashCode() & 0x7fffffff) % M;
+    }
+
+    public void put(Key key,Value val){
+        if(N >M/2) resize(2*M);
+        int i ;
+        for(i =hash(key);keys[i]!=null;i=(i+1)%M){
+            if(keys[i].equals(key)){
+                vals[i] = val;
+                return;
+            }
+        }
+        keys[i] = key;
+        vals[i] = val;
+        N++;
+    }
+
+    public Value get(Key key){
+        for (int i =hash(key);keys[i] !=null; i=(i+1)%M){
+            if(keys[i].equals(key)){
+                return vals[i];
+            }
+        }
+        return null;
+
+    }
+
+    public void delete(Key key){
+        if(!contains(key)) return;
+        int i = hash(key);
+        while(!key.equals(keys[i]))
+            i = (i +1 )% M;
+        keys[i] = null;
+        vals[i] = null;
+        i =(i + 1) % M;
+        while(keys[i] !=null){
+            Key keyToRedo = keys[i];
+            Value valToRedo = vals[i];
+            keys[i] = null;
+            vals[i] = null;
+            N--;
+            put(keyToRedo,valToRedo);
+            i = (i + 1) % M;
+        }
+        N--;
+        if(N >0 && N == M/8) resize(M/2);
+    }
+
+    private boolean contains(Key key) {
+
+        for (int i = 0; i < keys.length ; i++) {
+            if(keys[i].equals(key)) return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
