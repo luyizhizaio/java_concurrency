ca838d9961fa92f560b1b59583a2f707ec7215b0
changyue.li <changyue.li@tendcloud.com>
2019-09-17 17:38:02 +0800

    散列表-线性探测表
    Change-Id: I7811ef620386ae2de24c034a8857006781aadfea

--- src/main/java/com/kyrie/datastructure/search/LinearProbingHashST.java
+++ src/main/java/com/kyrie/datastructure/search/LinearProbingHashST.java
@@ -17,8 +17,22 @@ public class LinearProbingHashST<Key,Value> {
         vals = (Value[]) new Object[M];
     }
 
-    private void resize(int M){
+    public LinearProbingHashST(int cap){
+        this.M = cap;
+        keys = (Key[]) new Object[M];
+        vals = (Value[]) new Object[M];
+    }
 
+    private void resize(int cap){
+        LinearProbingHashST<Key,Value> t;
+        t = new LinearProbingHashST<Key,Value>(cap);
+        for(int i =0;i<M;i++){
+            if(keys[i] !=null)
+                t.put(keys[i],vals[i]);
+        }
+        keys = t.keys;
+        vals = t.vals;
+        M = t.M;
     }
 
     private int hash(Key key){
