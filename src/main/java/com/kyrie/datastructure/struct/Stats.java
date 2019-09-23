8d1865758c53a41054634e2667ed875598c4566d
changyue.li <changyue.li@tendcloud.com>
2019-09-23 15:59:10 +0800

    Bag sample
    Change-Id: I6447e32e0e0e8ba61928d9b67b6c5841101d1e92

---
+++ src/main/java/com/kyrie/datastructure/struct/Stats.java
@@ -0,0 +1,36 @@
+package com.kyrie.datastructure.struct;
+
+import com.kyrie.datastructure.utils.StdIn;
+import com.kyrie.datastructure.utils.StdOut;
+
+/**
+ * Created by tend on 2019/9/23.
+ * Bag的使用
+ */
+public class Stats {
+
+
+    public static void main(String[] args) {
+
+        Bag<Double> numbers = new Bag<Double>();
+        while(!StdIn.isEmpty())
+            numbers.add(StdIn.readDouble());
+        int N = numbers.size();
+
+        double sum = 0.0;
+        for(double x :numbers){
+            sum += x;
+        }
+        double  mean = sum/N;
+
+        sum =0.0;
+        for(double x : numbers)
+            sum+=(x - mean)*( x - mean);
+
+        double std = Math.sqrt(sum);
+
+        StdOut.println("mean:"+mean);
+        StdOut.println("std:"+std);
+
+    }
+}
