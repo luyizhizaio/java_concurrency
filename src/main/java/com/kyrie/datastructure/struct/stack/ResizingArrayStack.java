2638a5990a627617434ddaca228b20f6c389ebe3
changyue.li <changyue.li@tendcloud.com>
2019-09-29 17:57:22 +0800

    stack
    Change-Id: Ifa7e154d912cf3b33bf9f80b7931cf4234774cea

--- src/main/java/com/kyrie/datastructure/struct/stack/ResizingArrayStack.java
+++ src/main/java/com/kyrie/datastructure/struct/stack/ResizingArrayStack.java
@@ -4,6 +4,7 @@ import java.util.Iterator;
 
 /**
  * Created by tend on 2019/9/29.
+ *
  */
 public class ResizingArrayStack<Item> implements Iterable<Item> {
 
@@ -16,35 +17,54 @@ public class ResizingArrayStack<Item> implements Iterable<Item> {
         return N == 0;
     }
 
+    public void resize(int max){
+
+        Item [] temp = (Item[])new Object[max];
+        for (int i = 0; i <a.length ; i++) {
+            temp[i] = a[i];
+        }
+        a = temp;
+    }
 
     public void push(Item item){
-
-
+        if(N == a.length) resize(2 * a.length);
+        a[N++] = item;
 
     }
 
 
     public Item pop(){
-        return null;
+        Item item  =a[--N];
+        a[N] =null;
+
+        if(N >0 && N == a.length/4)resize(a.length / 2);
+        return  item;
     }
-
-
-
-
-
-
-
-
-
-
-
 
 
     @Override
     public Iterator<Item> iterator() {
-        return null;
+        return new ReverseArrayIterator();
     }
 
+    private int i = N;
+
+    private class ReverseArrayIterator implements  Iterator<Item>{
+
+        @Override
+        public boolean hasNext() {
+            return  i > 0;
+        }
+
+        @Override
+        public Item next() {
+            return a[--i];
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
 
 
 }
