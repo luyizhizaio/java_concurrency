c0d823595c6bce2bb0c9161b3b8405f91bddd2a3
changyue.li <changyue.li@tendcloud.com>
2020-01-02 22:27:58 +0800

    希尔排序

---
+++ src/main/java/com/kyrie/datastructure/sort/Shell.java
@@ -0,0 +1,36 @@
+package com.kyrie.datastructure.sort;
+
+/**
+ * Created by Kyrie on 2020/1/2.
+ * 希尔排序:对插入排序的改进。
+ */
+public class Shell extends BaseSort {
+
+    public static void main(String[] args){
+
+        Integer[] arr = {14,2,93,85,11,64,96,12,2,4};
+        sort(arr);
+        show(arr);
+    }
+
+    public static void sort(Comparable[] cp){
+        int N = cp.length;
+
+        int h =1;
+
+        while(h < N/3) h = h *3 +1; //1,4,13,121,364,...
+        while(h >=1) {
+
+            for (int i = h; i < N; i++) {
+                for (int j = i; j >=h ; j -= h) {
+                    //判断后一个是否小于前一个，如果小于则交换
+                    if (less(cp[j], cp[j - h])) exch(cp, j, j - h);
+                }
+            }
+            h = h/3;
+        }
+
+    }
+
+
+}
