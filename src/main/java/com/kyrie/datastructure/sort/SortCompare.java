1407c17abcbf83da3eb994af2244af645e72f696
changyue.li <changyue.li@tendcloud.com>
2020-01-13 18:44:42 +0800

    算法比较
    Change-Id: I94fb13e03124bc8703032757edc88b24003f07ee

--- src/main/java/com/kyrie/datastructure/sort/SortCompare.java
+++ src/main/java/com/kyrie/datastructure/sort/SortCompare.java
@@ -1,5 +1,7 @@
 package com.kyrie.datastructure.sort;
 
+import com.kyrie.datastructure.utils.StdOut;
+import com.kyrie.datastructure.utils.StdRandom;
 import org.apache.commons.lang3.time.StopWatch;
 
 /**
@@ -13,20 +15,42 @@ public class SortCompare {
         sw.start();
         if("Insertion".equals(alg)) Insertion.sort(a);
         if("Selection".equals(alg)) Selection.sort(a);
+        if("Merge".equals(alg)) Merge.sort(a);
+        if("Shell".equals(alg)) Shell.sort(a);
 
         return sw.getNanoTime();
     }
-    public static double timeRadomInput(){
+    public static double timeRadomInput(String alg ,int N ,int T){
 
+        //使用算法将T个长度为N的数组排序
+        double total = 0.0;
+        Double [] a = new Double [N];
+        for (int t = 0; t < T; t++) {
+            for (int i = 0; i < N; i++) {
+                a[i] = StdRandom.uniform();
+            }
+            total  += time(alg,a);
+        }
 
-        return 0.0;
+        return total;
     }
 
 
     public static void main(String [] args){
-        Integer[] arr = {14,2,93,85,11,64,96,12,2,4};
+       /* Integer[] arr = {14,2,93,85,11,64,96,12,2,4};
         System.out.println(time("Insertion", arr));
-        System.out.println(time("Selection", arr));
+        System.out.println(time("Selection", arr));*/
+
+        double t1 = timeRadomInput("Insertion" ,10000 ,1000);
+        double t2 = timeRadomInput("Selection" ,10000 ,1000);
+        double t3 = timeRadomInput("Merge" ,10000 ,1000);
+        double t4 = timeRadomInput("Shell" ,10000,1000);
+
+        StdOut.printf("Insertion:" + t1 + "\n");
+        StdOut.printf("Selection:" + t2 + "\n");
+        StdOut.printf("Merge:" + t3 + "\n");
+        StdOut.printf("Shell:" + t4 + "\n");
+
 
     }
 
