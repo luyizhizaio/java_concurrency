e8fa7c606ee0ab3a14a5d660cdb44f2e54125788
changyue.li <changyue.li@tendcloud.com>
2020-09-22 10:45:47 +0800

    归并排序
    Change-Id: I1e6524755a7633ee11dc3f2169cee7f8c3dcc163

--- src/main/java/com/kyrie/concurrency/forkjoin/ForkJoinTest.java
+++ src/main/java/com/kyrie/concurrency/forkjoin/ForkJoinTest.java
@@ -1,23 +1,26 @@
 package com.kyrie.concurrency.forkjoin;
 
+import org.junit.Test;
+
 import java.time.Duration;
 import java.time.Instant;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.ForkJoinTask;
 import java.util.concurrent.RecursiveTask;
+import java.util.stream.LongStream;
 
 /**
  * Created by tend on 2020/9/18.
  */
 public class ForkJoinTest {
 
-    public static void main(String[] args) {
-
+    @Test
+    public void FJTest() {
         Instant start = Instant.now();
         //create pool
         ForkJoinPool pool = new ForkJoinPool();
 
-        ForkJoinTask<Long> calculate = new ForkJoinSumCalculate(0,1000000L);
+        ForkJoinTask<Long> calculate = new ForkJoinSumCalculate(0,100000000L);
 
         //execute
         Long num = pool.invoke(calculate);
@@ -25,7 +28,32 @@ public class ForkJoinTest {
         System.out.println(num);
 
         Instant end = Instant.now();
-        System.out.println(Duration.between(end, start).toMillis());
+        System.out.println(Duration.between(start, end).toMillis());
+
+    }
+
+    /**
+     * jdk1.8新特性test方法
+     * JDK8 对 Fork/Join 的优化
+        JDK8 对 Fork/Join 的优化：主要是让 Fork/Join 使用起来更加方便。对 Fork/Join 进行了封装，简化使用方式。
+        对于 JDK8 对 Fork/Join 的底层优化，这里不进行分析。
+     */
+    @Test
+    public void test2(){
+
+
+        Instant start = Instant.now();
+
+        Long sum = LongStream.rangeClosed(0L, 50000000000L)
+                .parallel()
+                .reduce(0L, Long::sum);
+
+        System.out.println(sum);
+
+        Instant end = Instant.now();
+
+        System.out.println("耗费时间为：" + Duration.between(start, end).toMillis());//1536-8118
+
     }
 
 
@@ -38,7 +66,7 @@ class ForkJoinSumCalculate extends RecursiveTask<Long>{
     private long start;
     private long end;
 
-    private static final long THURSHOLDS = 10000; //阈值
+    private static final long THURSHOLDS = 10; //阈值
 
 
     public ForkJoinSumCalculate(long start, long end) {
@@ -51,7 +79,7 @@ class ForkJoinSumCalculate extends RecursiveTask<Long>{
 
         long length = end - start;
         //小于阈值开始相加
-        long sum = 0;
+        long sum = 0l;
         if(length <=THURSHOLDS){
 
             for (long i = start; i <=end ; i++) {
@@ -62,11 +90,11 @@ class ForkJoinSumCalculate extends RecursiveTask<Long>{
             long mid = (start+ end)/2;
 
             ForkJoinSumCalculate left = new ForkJoinSumCalculate(start, mid);
-            left.fork();//拆分
+            left.fork();//拆分，同时压入线程队列
 
 
             ForkJoinSumCalculate right = new ForkJoinSumCalculate(mid+1,end);
-            left.fork();//拆分
+            right.fork();//拆分
 
             return left.join() + right.join();//合并
         }
