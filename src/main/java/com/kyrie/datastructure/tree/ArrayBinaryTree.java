9c8221078882c3bbfebdabc8a16e22568971c77c
changyue.li <changyue.li@tendcloud.com>
2020-10-14 18:27:52 +0800

    二叉树-删除节点
    Change-Id: Id95edd3a254abe973ee3805b566e2fa094435e1a

--- src/main/java/com/kyrie/datastructure/tree/ArrayBinaryTree.java
+++ src/main/java/com/kyrie/datastructure/tree/ArrayBinaryTree.java
@@ -4,4 +4,18 @@ package com.kyrie.datastructure.tree;
  * Created by tend on 2020/10/14.
  */
 public class ArrayBinaryTree {
+
+    private int[] arr ;
+
+    public ArrayBinaryTree(int[] arr) {
+        this.arr = arr;
+    }
+
+
+    public void preOrder(){
+
+
+    }
+
+
 }
--- src/main/java/com/kyrie/datastructure/tree/ArrayBinaryTreeDemo.java
+++ src/main/java/com/kyrie/datastructure/tree/ArrayBinaryTreeDemo.java
@@ -1,12 +1,14 @@
 package com.kyrie.datastructure.tree;
 
+import org.junit.Test;
+
 /**
  * Created by tend on 2020/10/14.
  * 顺序存储二叉树
  */
 public class ArrayBinaryTreeDemo {
 
-
+    @Test
     public void traverse(){
 
     }
