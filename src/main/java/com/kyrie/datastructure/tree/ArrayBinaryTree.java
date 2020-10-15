4605b375a52b54c7a78a6e172548c712d41e75ec
changyue.li <changyue.li@tendcloud.com>
2020-10-15 10:39:24 +0800

    二叉树-删除节点
    Change-Id: If14cc058d45fe63fbbeefa7d375ce184840e1799

--- src/main/java/com/kyrie/datastructure/tree/ArrayBinaryTree.java
+++ src/main/java/com/kyrie/datastructure/tree/ArrayBinaryTree.java
@@ -2,6 +2,7 @@ package com.kyrie.datastructure.tree;
 
 /**
  * Created by tend on 2020/10/14.
+ * 顺序存储二叉树
  */
 public class ArrayBinaryTree {
 
@@ -11,11 +12,80 @@ public class ArrayBinaryTree {
         this.arr = arr;
     }
 
-
+    /**
+     * 先序遍历
+     */
     public void preOrder(){
+        preOrder(0);
+    }
 
+    private void preOrder(int rootIndex){
+
+        int n = arr.length;
+
+        System.out.print(arr[rootIndex] + " ");
+
+        //left
+        int leftIndex = 2 * rootIndex +1;
+        if(leftIndex < n) {
+            preOrder(leftIndex);
+        }
+
+        //right
+        int rightIndex = 2 * rootIndex +2;
+        if(rightIndex < n){
+            preOrder(rightIndex);
+        }
 
     }
 
+    /**
+     * 中序遍历
+     */
+    public void infixOrder() {
+        infixOrder(0);
+    }
 
+    private void infixOrder(int rootIndex) {
+
+        int n = arr.length;
+
+        //left
+        int leftIndex = 2 * rootIndex +1;
+        if(leftIndex < n) {
+            infixOrder(leftIndex);
+        }
+
+        System.out.print(arr[rootIndex] + " ");
+
+        //right
+        int rightIndex = 2 * rootIndex +2;
+        if(rightIndex < n){
+            infixOrder(rightIndex);
+        }
+    }
+
+    public void postOrder() {
+
+        postOrder(0);
+    }
+
+    private void postOrder(int rootIndex) {
+
+        int n = arr.length;
+
+        //left
+        int leftIndex = 2 * rootIndex +1;
+        if(leftIndex < n) {
+            postOrder(leftIndex);
+        }
+
+        //right
+        int rightIndex = 2 * rootIndex +2;
+        if(rightIndex < n){
+            postOrder(rightIndex);
+        }
+
+        System.out.print(arr[rootIndex] + " ");
+    }
 }
--- src/main/java/com/kyrie/datastructure/tree/ArrayBinaryTreeDemo.java
+++ src/main/java/com/kyrie/datastructure/tree/ArrayBinaryTreeDemo.java
@@ -4,12 +4,22 @@ import org.junit.Test;
 
 /**
  * Created by tend on 2020/10/14.
- * 顺序存储二叉树
+ * 顺序存储二叉树测试
  */
 public class ArrayBinaryTreeDemo {
 
     @Test
     public void traverse(){
 
+        int []arr = {1,2,3,4,5,6,7};
+        ArrayBinaryTree abt = new ArrayBinaryTree(arr);
+        System.out.println("preOrder:");
+        abt.preOrder(); //1 2 4 5 3 6 7
+
+        System.out.println("\ninfixOrder:");
+        abt.infixOrder();//4 2 5 1 6 3 7
+
+        System.out.println("\npostOrder:");
+        abt.postOrder(); //4 5 2 6 7 3 1
     }
 }
