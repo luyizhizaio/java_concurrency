a83d5316d144eff636ef615308435d8937a37ca2
changyue.li <changyue.li@tendcloud.com>
2020-10-26 11:44:40 +0800

    哈夫曼树
    Change-Id: I210838be1d4f8af3152ff4046e6fbf3cb0764480

--- src/main/java/com/kyrie/datastructure/treeApplication/HuffmanTree.java
+++ src/main/java/com/kyrie/datastructure/treeApplication/HuffmanTree.java
@@ -1,6 +1,7 @@
 package com.kyrie.datastructure.treeApplication;
 
-import java.util.Comparator;
+import java.util.*;
+
 
 /**
  * Created by tend on 2020/10/22.
@@ -9,31 +10,75 @@ public class HuffmanTree {
 
     public static void main(String[] args) {
 
+        int[] arr ={13,7,8,3,29,6,1};
+        Node huffmanTree = createHuffmanTree(arr);
+
+        System.out.println(huffmanTree);
 
 
+        preOrder(huffmanTree);
+
+    }
+
+    private static void preOrder(Node huffmanTree) {
+
+        huffmanTree.preOrder();
 
     }
 
 
-    public void  createHuffmanTree(int[] arr){
-        //对于给定的有各自权值的 n 个结点，构建哈夫曼树有一个行之有效的办法：
-//        在 n 个权值中选出两个最小的权值，对应的两个结点组成一个新的二叉树，且新二叉树的根结点的权值为左右孩子权值的和；
-//        在原有的 n 个权值中删除那两个最小的权值，同时将新的权值加入到 n–2 个权值的行列中，以此类推；
-//        重复 1 和 2 ，直到所以的结点构建成了一棵二叉树为止，这棵树就是哈夫曼树。
+    public static Node  createHuffmanTree(int[] arr){
+        /**对于给定的有各自权值的 n 个结点，构建哈夫曼树有一个行之有效的办法：
+         1.在 n 个权值中选出两个最小的权值，对应的两个结点组成一个新的二叉树，且新二叉树的根结点的权值为左右孩子权值的和；
+         2.在原有的 n 个权值中删除那两个最小的权值，同时将新的权值加入到 n–2 个权值的行列中，以此类推；
+         3.重复 1 和 2 ，直到所以的结点构建成了一棵二叉树为止，这棵树就是哈夫曼树。
+         */
+
+        //
+        ArrayList<Node> nodes = new ArrayList(arr.length);
+        for (int i : arr) {
+
+            nodes.add(new Node(i));
+        }
+
+        while(nodes.size()>1){
+
+            Collections.sort(nodes);
+            Node first = nodes.remove(0);
+            Node second = nodes.remove(0);
+            int parentValue = first.getValue() + second.getValue();
+            Node parentNode = new Node(parentValue);
+
+            parentNode.setLeft(first);
+            parentNode.setRight(second);
+            nodes.add(parentNode);
+
+        }
+
+        return nodes.get(0);
 
     }
+
 
 
 }
 
 
-class Node implements Comparator<Node>{
+class Node implements Comparable<Node>{
 
     private int value;
 
     private Node left;
 
     private Node right;
+
+    public Node(){
+
+    }
+
+    public Node(int value) {
+        this.value = value;
+    }
 
     public int getValue() {
         return value;
@@ -60,7 +105,29 @@ class Node implements Comparator<Node>{
     }
 
     @Override
-    public int compare(Node n1, Node n2) {
-        return n1.getValue() - n2.getValue();
+    public String toString() {
+        return "Node{" +
+                "value=" + value +
+                '}';
+    }
+
+    public void preOrder() {
+
+        System.out.print(this.getValue() +" ");
+        if(this.getLeft() !=null){
+            this.getLeft().preOrder();
+        }
+
+        if(this.getRight() != null){
+            this.getRight().preOrder();
+        }
+
+
+
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        return this.getValue() - o.getValue();
     }
 }
