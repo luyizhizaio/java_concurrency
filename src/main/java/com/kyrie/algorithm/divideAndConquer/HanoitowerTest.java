c49a6fdb8c02ca718f662128174dcd4a95adc154
changyue.li <changyue.li@tendcloud.com>
2020-11-24 17:44:14 +0800

    算法
    Change-Id: I9263dc0db2ea4b9d74cdce4b8a0a2591e36e739f

--- src/main/java/com/kyrie/algorithm/divideAndConquer/HanoitowerTest.java
+++ src/main/java/com/kyrie/algorithm/divideAndConquer/HanoitowerTest.java
@@ -5,5 +5,20 @@ package com.kyrie.algorithm.divideAndConquer;
  */
 public class HanoitowerTest {
 
+    public static void main(String[] args) {
+        hanoiTower(3,'A','B','C');
+    }
 
+    private static void hanoiTower(int num, char a, char b, char c) {
+        if(num ==1){
+            System.out.println("第1个盘从"+a+"->"+c);
+        }else{
+            //1.把最上面的所有盘从a->c,移动过程中会用到c
+            hanoiTower(num -1,a,c,b);
+            //2.把最下面的盘 a->c
+            System.out.println("第"+num+"个盘从"+a+"->"+c);
+            //3.把b塔的所有盘从b->c,移动过程中使用到a
+            hanoiTower(num -1,b,a,c);
+        }
+    }
 }
