1c404a7fe79d315451dcf5b4ec2451a96c9543c4
changyue.li <changyue.li@tendcloud.com>
2020-12-01 11:47:44 +0800

    马踏棋盘问题
    Change-Id: I727de51a48b028c5fd79e4d733579dde2b3e4206

---
+++ src/main/java/com/kyrie/algorithm/horseChessBoard/HorseChessBoard.java
@@ -0,0 +1,45 @@
+package com.kyrie.algorithm.horseChessBoard;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+/**
+ * 马踏棋盘问题
+ */
+public class HorseChessBoard {
+
+    private static int X;//棋盘的列数
+    private static int Y;//棋盘的行数
+
+    //创建一个数组，标记棋盘的各个位置是否被访问过
+    private static boolean visited[];
+
+    private static boolean finished; //如果为true，表示成功
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+    /**
+     * 根据当前位置，获取下一个位置
+     * @param curPoint
+     * @return
+     */
+    public static ArrayList<Point> next(Point curPoint){
+        ArrayList<Point> ps = new ArrayList<>();
+
+        Point p1 = new Point();
+
+        if()
+
+    }
+
+}
